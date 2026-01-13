c2bd7b0a797920429ba5f140f02aeff97cada850
Erik O Gabrielsson <erik.o.gabrielsson@sectra.com>
2026-01-13 08:50:06 +0100

    Do not return until keepalive resolved

--- slidetap-client/src/services/auth.ts
+++ slidetap-client/src/services/auth.ts
@@ -64,12 +64,11 @@ const BasicAuth = {
 
   keepAlive: () => {
     if (!getLoggedIn()) return false
-    loginApi.keepAlive().catch((x) => {
+    return loginApi.keepAlive().then(() => true).catch((x) => {
       console.error('Failed to send keep alive. Logging out user', x)
       BasicAuth.logout()
       return false
     })
-    return true
   },
 
   /**
